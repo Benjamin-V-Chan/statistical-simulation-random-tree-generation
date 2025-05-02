19842448636106347bcce7cf7dc86fa8a57402f3
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-02 12:29:38 -0700

    script updates

--- scripts/02_compute_statistics.py
+++ scripts/02_compute_statistics.py
@@ -8,12 +8,14 @@ def compute_metrics(tree):
     G = nx.Graph()
     G.add_edges_from(tree["edges"])
     n = tree["n_nodes"]
+    
     # height from node 1
     lengths = nx.single_source_shortest_path_length(G, 1)
     height = max(lengths.values())
     diameter = nx.diameter(G)
     avg_degree = sum(dict(G.degree()).values()) / n
     avg_path_length = nx.average_shortest_path_length(G)
+    
     return {
         "tree_id": tree["tree_id"],
         "n_nodes": n,
