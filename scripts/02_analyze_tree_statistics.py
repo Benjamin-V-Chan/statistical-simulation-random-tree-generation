5301cd54e5913424c72d7696083097c8d7f2eba1
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-01 21:12:50 -0700

    02_analyze_tree_statistics script outline

---
+++ scripts/02_analyze_tree_statistics.py
@@ -0,0 +1,15 @@
+# Script to analyze basic statistics of a tree
+
+# Imports
+# - networkx
+# - numpy
+
+# Define function analyze_tree(G):
+#   Input: G (tree as networkx graph)
+#   Output: dict with:
+#       - number of nodes
+#       - number of leaves
+#       - average degree
+#       - maximum degree
+#       - diameter of tree
+#       - average path length
