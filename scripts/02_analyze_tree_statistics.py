cd01524128b1877bb9a2cce212634083b2392c2d
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-01 21:12:58 -0700

    02_analyze_tree_statistics script complete

--- scripts/02_analyze_tree_statistics.py
+++ scripts/02_analyze_tree_statistics.py
@@ -1,15 +1,20 @@
-# Script to analyze basic statistics of a tree
+import networkx as nx
+import numpy as np
 
-# Imports
-# - networkx
-# - numpy
-
-# Define function analyze_tree(G):
-#   Input: G (tree as networkx graph)
-#   Output: dict with:
-#       - number of nodes
-#       - number of leaves
-#       - average degree
-#       - maximum degree
-#       - diameter of tree
-#       - average path length
+def analyze_tree(G):
+    num_nodes = G.number_of_nodes()
+    num_leaves = sum(1 for node in G.nodes() if G.degree(node) == 1)
+    degrees = [G.degree(node) for node in G.nodes()]
+    avg_degree = np.mean(degrees)
+    max_degree = np.max(degrees)
+    diameter = nx.diameter(G)
+    avg_path_length = nx.average_shortest_path_length(G)
+    
+    return {
+        'num_nodes': num_nodes,
+        'num_leaves': num_leaves,
+        'avg_degree': avg_degree,
+        'max_degree': max_degree,
+        'diameter': diameter,
+        'avg_path_length': avg_path_length
+    }
